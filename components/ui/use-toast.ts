4c2157473bab7dd6c563bc90933af67ccf4d6282
lopomkt <bepuree@gmail.com>
2025-04-19 22:07:52 -0400

    Delete components directory

--- components/ui/use-toast.ts
+++
@@ -1,5 +0,0 @@
-
-import { useToast as useHookToast, toast as hookToast } from "@/hooks/use-toast";
-
-// Re-export the hook and toast function
-export { useHookToast as useToast, hookToast as toast };
